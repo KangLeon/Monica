26c7c76ca1bc14895ceca0adec0a05e399f63aa2
Leon Kang <kangleonjy@gmail.com>
2020-08-13 23:40:12 +0800

    New Project

---
+++ Monica/Monica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Monica
+//
+//  Created by 吉腾蛟 on 2020/8/13.
+//  Copyright © 2020 jiyoung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Monica/MonicaTests/MonicaTests.swift
@@ -0,0 +1,34 @@
+//
+//  MonicaTests.swift
+//  MonicaTests
+//
+//  Created by 吉腾蛟 on 2020/8/13.
+//  Copyright © 2020 jiyoung. All rights reserved.
+//
+
+import XCTest
+@testable import Monica
+
+class MonicaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
